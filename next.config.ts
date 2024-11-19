56c9ff9ba56c04843d70de7d76cbc7d073c5d907
mohitM0 <mohit@m0.ventures>
2024-11-19 15:21:01 +0530

    modified config file to allow frame embedding

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,33 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // prevent double render on dev mode, which causes 2 frames to exist
+  reactStrictMode: false,
+  images: {
+    minimumCacheTTL: 1, // to allow dynamic images in case you are previewing them using next/image
+    remotePatterns: [
+      {
+        hostname: "*",
+        protocol: "http",
+      },
+      {
+        hostname: "*",
+        protocol: "https",
+      },
+    ],
+  },
+  async headers() {
+    return [
+      {
+        source: "/:path*", // Apply to all paths
+        headers: [
+          {
+            key: "Content-Security-Policy",
+            value: "frame-ancestors 'self' *", // Allow iframe embedding
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
