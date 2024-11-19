7134073710c3765e0106a4be8fc573bbd486fefb
mohitM0 <mohit@m0.ventures>
2024-11-19 17:27:28 +0530

    testing

--- next.config.ts
+++ next.config.ts
@@ -1,37 +1,7 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  // Prevent double render on dev mode, which causes 2 frames to exist
-  reactStrictMode: false,
-  images: {
-    minimumCacheTTL: 1, // To allow dynamic images in case you are previewing them using next/image
-    remotePatterns: [
-      {
-        hostname: "*",
-        protocol: "http",
-      },
-      {
-        hostname: "*",
-        protocol: "https",
-      },
-    ],
-  },
-  async headers() {
-    return [
-      {
-        source: "/:path*", // Apply to all paths
-        headers: [
-          {
-            key: "Content-Security-Policy",
-            value: "frame-ancestors 'self' *", // Allow iframe embedding
-          },
-          {
-            key: "X-Frame-Options",
-            value: "ALLOW-FROM http://localhost:3000", // Remove the header or explicitly set it to allow
-          },
-        ],
-      },
-    ];
-  },
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* config options here */
 };
 
 export default nextConfig;
