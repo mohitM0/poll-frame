0e853bfb2a3d22c0acffe28e6dbc8d5504fc37e0
mohitM0 <mohit@m0.ventures>
2024-11-19 10:47:13 +0530

    refactored code for testing

--- pages/api/actions/create-poll.ts
+++ pages/api/actions/create-poll.ts
@@ -11,9 +11,9 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
             : "http://localhost:3000"
     );
 
-    res.setHeader("Access-Control-Allow-Origin",req.headers.origin || "*");
-    res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
-    res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+    // res.setHeader("Access-Control-Allow-Origin",req.headers.origin || "*");
+    // res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+    // res.setHeader("Access-Control-Allow-Headers", "Content-Type");
 
     if (req.method === "GET") {
         try {
